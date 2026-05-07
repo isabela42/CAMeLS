24ba370ecd1794dcbfdb2d99b63dc7a607560685
Isabela Almeida <mb.isabela42@gmail.com>
2026-05-07 17:48:53 +1000

    043 outdir path correct for subfolders

--- scripts/ipda_camels_step043-to-pbs.sh
+++ scripts/ipda_camels_step043-to-pbs.sh
@@ -266,7 +266,7 @@ cut -f1 ${input} | sort | uniq | while read stem; do in_stem=`grep "${stem}" ${i
 cut -f1 ${input} | sort | uniq | while read stem; do echo "" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
 
 cut -f1 ${input} | sort | uniq | while read stem; do echo 'echo "## Plot results at" ; date ; echo' >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
-cut -f1 ${input} | sort | uniq | while read stem; do rscript=`grep "${stem}" ${input} | cut -f6 | sort | uniq`; rfunctions=`grep "${stem}" ${input} | cut -f7 | sort | uniq`; combined=`grep "${stem}" ${input} | cut -f8 | sort | uniq`; replicates=`grep "${stem}" ${input} | cut -f9 | sort | uniq`; echo "Rscript ${rscript} --inputgene ${out_path_step043_summary}/${stem}.gene_summary.full.tsv --inputsgrna ${out_path_step043_summary}/${stem}.sgrna_summary.full.tsv --inputcomb ${combined} --inputrep ${replicates} --outdir ${out_path_step043_summary} --outstem ${stem} --function ${rfunctions}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
+cut -f1 ${input} | sort | uniq | while read stem; do rscript=`grep "${stem}" ${input} | cut -f6 | sort | uniq`; rfunctions=`grep "${stem}" ${input} | cut -f7 | sort | uniq`; combined=`grep "${stem}" ${input} | cut -f8 | sort | uniq`; replicates=`grep "${stem}" ${input} | cut -f9 | sort | uniq`; echo "Rscript ${rscript} --inputgene ${out_path_step043_summary}/${stem}.gene_summary.full.tsv --inputsgrna ${out_path_step043_summary}/${stem}.sgrna_summary.full.tsv --inputcomb ${combined} --inputrep ${replicates} --outdir ${out_path_step043_summary}/ --outstem ${stem} --function ${rfunctions}" >> ${pbs_stem}_${stem}_${thislogdate}.pbs; done
 
 #................................................
 #  Submit PBS jobs
